d311a3fd16ecb4cc04fff98ab098f343235b960c
VicLabadesa04 <125319223+VicLabadesa04@users.noreply.github.com>
2023-03-20 22:46:39 -0300

    aula 20.03

--- Exemplos JS/005-atividade.js
+++ Exemplos JS/005-atividade.js
@@ -1,35 +1,5 @@
-//crie um programa que recba 02 nota de 0 a 10 
-// e  exiba as seguintes mensagens para cada faixa de valor
-// Nota <5 =I; 
-// Nota >= 5 e < 6,5 =R;
-// Nota >6,5 < 8,5  = B;
-// Nota >= 8,5= MB;
-
-// if (nota>=5 && nota <6.5 {
-   let.entrada= require ('prompt-sync') ();
-   console.log( 'programa de cálculo de notas da ETEC');
-   let valor;
-
-   valor = entrada ('Digite a 1° nota do seu trabalho:');
-let n1= parseFloat (valor);
-
-   valor= entrada ('Digite a 2° nota do seu trabalho:');
-   let n2= parseFloat (valor);
-
-   let media= (n1+n2) / 2;
-
-if (media < 5) {
-valor = "I";
-}
-if (media>= 5 && media < 6.5) {
-valor = "R"
-}
-if (media >= 6.5 && media 8.5 ){
-   Valor="B";
-}
-if (media >=8.5){
-   valor = "MB"
-}
-console.log('A nota Final é: ' + valor);
-
+// Crie um programa que receba duas notas de 0 a 10
+// e exiba as seguintes mensagens para cada faixa de valores
+// nota < 5 = I, nota >= 5 e < 6.5 = R, nota > 6.5 e < 8.5 = B 
+// e nota >= 8.5 MB
 
